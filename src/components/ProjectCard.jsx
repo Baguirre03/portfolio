a06146bbd9881b12100ed0ddfd65ac74ef77f487
Ben <baguirre@luc.edu>
2023-10-04 11:33:54 -0500

    final butotn styling

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,21 +2,53 @@
 export default function ProjectCard({ data }) {
   return (
     <div className="project-card">
-      <h4>{data.title}</h4>
       <img className="project-image" src={data.imgSrc} alt="" />
-      <ul>
-        Tools:{" "}
-        {data.tools.map((tool) => (
-          <li key={tool}>{tool}</li>
-        ))}
-      </ul>
-      <p>{data.description}</p>
-      <button>
-        <a href={data.repo}>Repository</a>
-      </button>
-      <button>
-        <a href={data.live}>Live</a>
-      </button>
+      <div className="project-right">
+        <h4 className="project-title">{data.title}</h4>
+        <ul className="project-tools">
+          <b>Built with:</b>
+          {data.tools}
+        </ul>
+        <p className="project-description">{data.description}</p>
+        <div className="project-buttons">
+          <button className="project-btn">
+            <a
+              className="project-a"
+              href={data.repo}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Repo
+              <svg
+                className="button-svg"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <title>github</title>
+                <path d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z" />
+              </svg>
+            </a>
+          </button>
+          <button className="project-btn">
+            <a
+              className="project-a"
+              href={data.live}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Live
+              <svg
+                className="button-svg"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <title>link</title>
+                <path d="M3.9,12C3.9,10.29 5.29,8.9 7,8.9H11V7H7A5,5 0 0,0 2,12A5,5 0 0,0 7,17H11V15.1H7C5.29,15.1 3.9,13.71 3.9,12M8,13H16V11H8V13M17,7H13V8.9H17C18.71,8.9 20.1,10.29 20.1,12C20.1,13.71 18.71,15.1 17,15.1H13V17H17A5,5 0 0,0 22,12A5,5 0 0,0 17,7Z" />
+              </svg>
+            </a>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,7 @@ const projects = [
   {
     title: "Attack on Titan Memory Card Game",
     imgSrc: aot,
-    tools: ["React", "Vite", "CSS"],
+    tools: "React, Vite, CSS",
     description:
       "Developed a memory card game using an Attack on Titan API using React concepts such as useEffect for API integration and useState for dynamic DOM updates",
     repo: "https://github.com/Baguirre03/memory-card",
@@ -17,7 +17,7 @@ const projects = [
   {
     title: "Battleship",
     imgSrc: battleship,
-    tools: ["JavaScript", "CSS", "HTML"],
+    tools: "JavaScript, CSS, HTML",
     description:
       "Battleship game made using TDD. Built a smart AI to play against, and interactive gameboard",
     repo: "https://github.com/Baguirre03/battleship",
@@ -26,7 +26,7 @@ const projects = [
   {
     title: "CV-Application",
     imgSrc: cvApp,
-    tools: ["React", "Vite", "CSS"],
+    tools: "React, Vite, CSS",
     description:
       "Developed a CV application  that dynamically updates a mock resume in real-time as users inputer their information. Implemented using React and Vite while leveraged useState. Components used allowed in code-resuability",
     repo: "https://github.com/Baguirre03/cv-appp",
