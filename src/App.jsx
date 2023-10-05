0282c1dbdb4ddd673afbbf49b35754163b2f596b
Ben <baguirre@luc.edu>
2023-10-05 14:00:55 -0500

    final contact section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Name from "./components/Name";
 import About from "./components/About";
 import Tools from "./components/Tools";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 import "./styles/app.css";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Tools name="tools"></Tools>
         <Projects name="projects"></Projects>
       </main>
+      <Contact name="contact"></Contact>
     </>
   );
 }
